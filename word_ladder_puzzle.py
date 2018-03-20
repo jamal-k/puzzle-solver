3ac303e27b9ca11b4f86f7c292b418c3efd6679f
jamal-k <33435065+jamal-k@users.noreply.github.com>
2018-03-19 23:15:14 -0400

    Update word_ladder_puzzle.py

--- word_ladder_puzzle.py
+++ word_ladder_puzzle.py
@@ -22,10 +22,6 @@ class WordLadderPuzzle(Puzzle):
         # set of characters to use for 1-character changes
         self._chars = "abcdefghijklmnopqrstuvwxyz"
 
-        # TODO
-        # implement __eq__ and __str__
-        # __repr__ is up to you
-
     def __eq__(self, other):
         """
         Return whether WordLadderPuzzle self is equivalent to other.
@@ -65,12 +61,6 @@ class WordLadderPuzzle(Puzzle):
         """
         return "{} -> {}".format(self._from_word, self._to_word)
 
-        # TODO
-        # override extensions
-        # legal extensions are WordLadderPuzzles that have a from_word that can
-        # be reached from this one by changing a single letter to one of those
-        # in self._chars
-
     def extensions(self):
         """
         Return list of extensions of WordLadderPuzzle self.
@@ -99,11 +89,6 @@ class WordLadderPuzzle(Puzzle):
                                                 self._word_set))
 
         return lst
-
-        # TODO
-        # override is_solved
-        # this WordLadderPuzzle is solved when _from_word is the same as
-        # _to_word
 
     def is_solved(self):
         """
